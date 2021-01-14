8f6032c02195af1a4126b9ffa363e147adc8ede6
sachinm78 <sachinm78@gmail.com>
2021-01-14 15:41:39 -0600

    adjusting logo and which side sidemenu toggle button will appear; logo needs clean-up work

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { FaBars } from 'react-icons/fa';
 import { useGlobalContext } from '../context';
+import logo from '../logo.png';
 
 const Home = () => {
   const { openSidebar } = useGlobalContext();
   return (
     <main>
+      <img src={logo} className='logo-home' alt='sachinm78' />
       <button onClick={openSidebar} className='sidebar-toggle'>
         <FaBars />
       </button>
