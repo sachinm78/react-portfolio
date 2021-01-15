a88f8fc1710869a0fe4cfb1b11f4c153886e06f1
sachinm78 <sachinm78@gmail.com>
2021-01-15 13:07:32 -0600

    created generic about page; added some basic styling but it's conflicting with the logo styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Sidebar from './components/Sidebar'
-import Home from './pages/Home' 
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
     <>
       <Home />
+      {/* <About /> */}
       <Sidebar />
     </>
   );
